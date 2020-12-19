2bc377668d65761fe273711c40345039458172c2
Chris Cowley <chris.cowley@fr.clara.net>
2020-12-19 01:01:53 +0100

    Updata prometheus architecture diagram to add HA

--- architecture/monitoring/prometheus_mvp.py
+++ architecture/monitoring/prometheus_mvp.py
@@ -7,6 +7,7 @@ from diagrams.aws.network import ELB
 from diagrams.onprem.compute import Server
 from diagrams.onprem.iac import Ansible
 from diagrams.onprem.monitoring import Grafana, Prometheus
+from diagrams.onprem.network import HAProxy
 from diagrams.saas.alerting import Pushover
 from diagrams.saas.chat import Slack
 
@@ -17,46 +18,59 @@ node_attr = {
         }
 
 with Diagram("Prometheus MVP",
-             show=False,
+             show=True,
              direction="TB",
              outformat="png",
              graph_attr=graph_attr,
              node_attr=node_attr):
 
     with Cluster("Rocky VPC"):
+        with Cluster("Dashboard"):
+            dashboard = Grafana("dashboards")
+            loadbalancer = HAProxy("grfana")
+            dashboard >> loadbalancer
+
+        with Cluster("metrics cluster"):
+            metrics = [
+                    Prometheus("metrics01") >> Prometheus("alertmanger01"),
+                    Prometheus("metrics02") >> Prometheus("alertmanger02"),
+                    ]
+
         with Cluster("AWS services"):
             aws_group = [
-                    EC2("service01"),
-                    EC2("service02"),
+                    EC2("service01") << Edge(style="dotted",
+                                             color="red") << metrics,
+                    EC2("service02") << Edge(style="dotted",
+                                             color="red") << metrics,
                     ]
-        with Cluster("metrics host"):
-            metrics = Prometheus("metrics")
-            alertmanager = Prometheus("alertmanager")
-            dashboard = Grafana("monitoring")
-            metrics << dashboard
-            metrics >> alertmanager
+
+    loadbalancer >> metrics
 
     Ansible("ansible") >> metrics
     metrics >> Edge(style="dashed",
                     label="ec2 read permissions") >> General("AWS API")
 
-    alertmanager >> Edge(style="dashed",
+    metrics >> Edge(style="dashed",
                          label="non-critical") >> Slack("rocky-alerts")
-    alertmanager >> Edge(style="dashed",
+    metrics >> Edge(style="dashed",
                          label="critical") >> Pushover("tbd")
     ELB("metrics.rockylinux.org") >> Edge(label="TCP3000") >> dashboard
     with Cluster("Cloudvider"):
         cloudvider_group = [
-                Server("server01"),
-                Server("server02"),
+                Server("server01") << Edge(style="dotted",
+                                           color="red") << metrics,
+                Server("server02") << Edge(style="dotted",
+                                           color="red") << metrics,
                 ]
 
     with Cluster("Spry Servers"):
         spry_group = [
-                Server("server01"),
-                Server("server02"),
+                Server("server01") << Edge(style="dotted",
+                                           color="red") << metrics,
+                Server("server02") << Edge(style="dotted",
+                                           color="red") << metrics,
                 ]
 
-    metrics >> aws_group
-    metrics >> spry_group
-    metrics >> cloudvider_group
+    # metrics >> aws_group
+    # metrics >> spry_group
+    # metrics >> cloudvider_group
